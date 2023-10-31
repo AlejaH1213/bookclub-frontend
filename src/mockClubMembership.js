cc015499598e19101e2f5842902da5245cc9541f
alopezdelgado <andrealdelgado_99@outlook.com>
2023-10-31 16:00:57 -0700

    added mock club membership file

---
+++ src/mockClubMembership.js
@@ -0,0 +1,27 @@
+let membership = [
+    {
+        id: 1,
+        club_id: 1,
+        user_id: 1,
+    },
+    {
+        id: 2,
+        club_id: 1, 
+        user_id: 2,
+    },
+    {
+        id: 3, 
+        club_id: 1, 
+        user_id: 3
+    }, 
+    {
+        id: 4,
+        club_id: 2,
+        user_id: 4,
+    },
+    {
+        id: 5,
+        club_id: 2,
+        user_id: 5
+    }
+]
