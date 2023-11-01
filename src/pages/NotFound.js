f175547800617b6b5f5fa51e46ca2fa81e1e8221
alopezdelgado <andrealdelgado_99@outlook.com>
2023-11-01 16:19:09 -0700

    home and not found styled and functional, updated read me

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import notFoundPic from '../assets/image-from-rawpixel-id-10092909-png.png'
 
 function NotFound() {
   return (
-    <div>NotFound</div>
+    <div className='not-found'>
+    <h2 >Not Found Try Again</h2>
+    <img 
+    src={notFoundPic}
+    style={{
+      height: '600px',
+      width: '600px'
+    }}
+    />
+  </div>
   )
 }
 
