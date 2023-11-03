fa7e70a0a362a3a93c40dac6f9cf720dc0fe6f0f
alopezdelgado <andrealdelgado_99@outlook.com>
2023-11-03 14:37:59 -0700

    made indenting even better

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -9,113 +9,113 @@ const AboutUs = () => {
     <>
       <div className='cards-container'>
         <div className='aleja-card'>
-        <Card
-        style={{
-          width: '18rem',
-          backgroundColor: '#ff0000'
-        }}
-        >
-          <CardBody>
-            <CardTitle tag="h5">
-              Aleja Hale
-            </CardTitle>
-            <CardSubtitle
-            tag="h6"
-              >
-              Project, Product Manager
-            </CardSubtitle>
-          </CardBody>
-          <img
-          alt="Card cap"
-          src={Aleja}
-          width="100%"
-          />
-          <CardBody>
-            <CardText>
-            Aleja is a full stack web developer based in California, with experience in multiple programming languages such as React, Ruby and Javascript. Rapidly learning new skills and embracing various work environments are among her strengths
-            </CardText>
-            <CardLink className='about-us-link' href="https://www.linkedin.com/in/alejandra-hale/" target='_blank'>
-              LinkedIn
-            </CardLink>
-            <CardLink className='about-us-link' href="#">
-              GitHub
-            </CardLink>
-          </CardBody>
-        </Card>
-      </div>
-      <div className='xe-card'>
-        <Card
-          style={{
-            width: '18rem',
-            backgroundColor: '#c3c205'
-          }}
-          className='indiv-card'
-        >
-          <CardBody>
-            <CardTitle tag="h5">
-              Xena Sitnikova
-            </CardTitle>
-            <CardSubtitle
-              tag="h6"
-              >
-              Tech Lead
-            </CardSubtitle>
-          </CardBody>
-          <img
-            alt="Card cap"
-            src={Xena}
-            width="100%"
-          />
-          <CardBody>
-            <CardText>
-              Hey, I'm Xena, and I tackle the daily grind with a sprinkle of excitement, much like I add extra shots of espresso to my coffee. I'm like a bookworm on a caffeine high, making each day a hilarious adventure, one page – I mean step – at a time!
-            </CardText>
-            <CardLink className='about-us-link' href="https://www.linkedin.com/in/sxena/" target='_blank'>
-              LinkedIn
-            </CardLink>
-            <CardLink className='about-us-link' href="https://github.com/XenaSit" target='_blank'>
-              GitHub
-            </CardLink>
-          </CardBody>
-        </Card>
-      </div>
-      <div className='dre-card'>
-        <Card
-          style={{
-            width: '18rem',
-            backgroundColor: '#062e57',
-            color: 'black'
-          }}
-          className='indiv-card'
+          <Card
+            style={{
+              width: '18rem',
+              backgroundColor: '#ff0000'
+            }}
           >
-          <CardBody>
-            <CardTitle tag="h5">
-              Dre Lopez Delgado
-            </CardTitle>
-            <CardSubtitle
-              tag="h6"
+            <CardBody>
+              <CardTitle tag="h5">
+                Aleja Hale
+              </CardTitle>
+              <CardSubtitle
+                tag="h6"
               >
-              Design Lead
-            </CardSubtitle>
-          </CardBody>
-          <img
-            alt="Card cap"
-            src={Dre}
-            width="100%"
+                Project, Product Manager
+              </CardSubtitle>
+            </CardBody>
+            <img
+              alt="Card cap"
+              src={Aleja}
+              width="100%"
             />
-          <CardBody>
-            <CardText>
-              Dre is a big science nerd with a love for tech and books. They are a full stack web developer with skills such as React.js and Ruby on Rails. You can usually find them typing away at their desk or reading a book with a coffee on hand. 
-            </CardText>
-            <CardLink className='about-us-link' href="https://www.linkedin.com/in/andrea-lopez-/" target='_blank'>
-              LinkedIn
-            </CardLink>
-            <CardLink className='about-us-link' href="https://github.com/alopezdelgado" target='_blank'>
-              GitHub
-            </CardLink>
-          </CardBody>
-        </Card>
-      </div>
+            <CardBody>
+              <CardText>
+                Aleja is a full stack web developer based in California, with experience in multiple programming languages such as React, Ruby and Javascript. Rapidly learning new skills and embracing various work environments are among her strengths
+              </CardText>
+              <CardLink className='about-us-link' href="https://www.linkedin.com/in/alejandra-hale/" target='_blank'>
+                LinkedIn
+              </CardLink>
+              <CardLink className='about-us-link' href="#">
+                GitHub
+              </CardLink>
+            </CardBody>
+          </Card>
+        </div>
+        <div className='xe-card'>
+          <Card
+            style={{
+              width: '18rem',
+              backgroundColor: '#c3c205'
+            }}
+            className='indiv-card'
+          >
+            <CardBody>
+              <CardTitle tag="h5">
+                Xena Sitnikova
+              </CardTitle>
+              <CardSubtitle
+                tag="h6"
+              >
+                Tech Lead
+              </CardSubtitle>
+            </CardBody>
+            <img
+              alt="Card cap"
+              src={Xena}
+              width="100%"
+            />
+            <CardBody>
+              <CardText>
+                Hey, I'm Xena, and I tackle the daily grind with a sprinkle of excitement, much like I add extra shots of espresso to my coffee. I'm like a bookworm on a caffeine high, making each day a hilarious adventure, one page – I mean step – at a time!
+              </CardText>
+              <CardLink className='about-us-link' href="https://www.linkedin.com/in/sxena/" target='_blank'>
+                LinkedIn
+              </CardLink>
+              <CardLink className='about-us-link' href="https://github.com/XenaSit" target='_blank'>
+                GitHub
+              </CardLink>
+            </CardBody>
+          </Card>
+        </div>
+        <div className='dre-card'>
+          <Card
+            style={{
+              width: '18rem',
+              backgroundColor: '#062e57',
+              color: 'black'
+            }}
+            className='indiv-card'
+          >
+            <CardBody>
+              <CardTitle tag="h5">
+                Dre Lopez Delgado
+              </CardTitle>
+              <CardSubtitle
+                tag="h6"
+              >
+                Design Lead
+              </CardSubtitle>
+            </CardBody>
+            <img
+              alt="Card cap"
+              src={Dre}
+              width="100%"
+              />
+            <CardBody>
+              <CardText>
+                Dre is a big science nerd with a love for tech and books. They are a full stack web developer with skills such as React.js and Ruby on Rails. You can usually find them typing away at their desk or reading a book with a coffee on hand. 
+              </CardText>
+              <CardLink className='about-us-link' href="https://www.linkedin.com/in/andrea-lopez-/" target='_blank'>
+                LinkedIn
+              </CardLink>
+              <CardLink className='about-us-link' href="https://github.com/alopezdelgado" target='_blank'>
+                GitHub
+              </CardLink>
+            </CardBody>
+          </Card>
+       </div>
       </div>
     </>
 
