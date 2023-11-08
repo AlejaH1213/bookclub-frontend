e33ae09590285e9ba02d3518cedddbbdc5bffa95
XenaSit <sitnikovaxena@gmail.com>
2023-11-08 15:02:13 -0800

    did the indententions

--- src/pages/YourClubsIndex.js
+++ src/pages/YourClubsIndex.js
@@ -9,49 +9,49 @@ const YourClubsIndex = ({currentUser, bookClubs, memberships, deleteBookClub, de
     return bookClubs.find(bookClub => bookClub.id === membership.club_id)
   })
 
-const handleDelete = (id) => {
-  deleteBookClub(id);
-  window.location.reload()
-}
+  const handleDelete = (id) => {
+    deleteBookClub(id);
+    window.location.reload()
+  }
 
   console.log("userBookClubs:", userBookClubs);
   console.log("userMemberships:", userMemberships);
   return (
     <>
-    <main>
-      <h2>Here are your clubs</h2>
-      {userBookClubs && (userBookClubs?.map((bookClub, index) => (
-        <Card
-          style={{
-            width: '18rem'
-          }}
-          key={index}
-          className="card-item"
-        >
-          <CardImg
-            alt={`profile picture for ${bookClub?.name}`}
-            src={bookClub?.book_of_the_month_picture}
-          />
-          <CardBody>
-            <CardTitle tag="h5">
-              {`${bookClub?.summary}`}
-            </CardTitle>
-            <Button>
-              <NavLink to={`/clubs/${bookClub?.id}`} className="nav-link">
-                See more details!
+      <main>
+        <h2>Here are your clubs</h2>
+        {userBookClubs && (userBookClubs?.map((bookClub, index) => (
+          <Card
+            style={{
+              width: '18rem'
+            }}
+            key={index}
+            className="card-item"
+          >
+            <CardImg
+              alt={`profile picture for ${bookClub?.name}`}
+              src={bookClub?.book_of_the_month_picture}
+            />
+            <CardBody>
+              <CardTitle tag="h5">
+                {`${bookClub?.summary}`}
+              </CardTitle>
+              <Button>
+                <NavLink to={`/clubs/${bookClub?.id}`} className="nav-link">
+                  See more details!
+                </NavLink>
+              </Button>
+              <Button>
+                <NavLink to={`/clubedit/${bookClub?.id}`} className="nav-link">
+                  Edit
+                </NavLink>
+              </Button>
+              <NavLink>
+                <Button onClick={() => handleDelete(bookClub?.id)}>Delete</Button>
               </NavLink>
-            </Button>
-            <Button>
-              <NavLink to={`/clubedit/${bookClub?.id}`} className="nav-link">
-                Edit
-              </NavLink>
-            </Button>
-            <NavLink>
-              <Button onClick={() => handleDelete(bookClub?.id)}>Delete</Button>
-            </NavLink>
-          </CardBody>
-        </Card>
-      )))}
+            </CardBody>
+          </Card>
+        )))}
       </main>
     </>
   );
